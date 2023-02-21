46338e0bc62467b42aee8851d0702b5b69da5b14
Iris D <iris.dijkstra@hotmail.com>
2023-02-21 15:29:19 +0100

    Rails Task Manager (before refactor)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Overview of all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Read(one) task (READ)
+  get 'task/:id', to: 'tasks#task'
+
+  # Add(one) task (CREATE)
+  get 'tasks/add', to: 'tasks#add'
+  post 'tasks', to: 'tasks#upload'
+
+  # Update(one) task (UPDATE)
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'task/:id', to: 'tasks#update', as: :update
+
+  # Destroy(one) task (DELETE)
+  get 'task/:id/destroy', to: 'tasks#destroy', as: :destroy
 end
---
+++ workflow.md
@@ -0,0 +1,12 @@
+
+*** User actions ***
+
+[ ] As a user, I can list tasks
+[ ] As a user, I can view the details of a task
+[ ] As a user, I can add a new task
+[ ] As a user, I can edit a task (mark as completed / update title & details)
+[ ] As a user, I can remove a task
+
+*** Coding steps ***
+
+
